42d735fe842c692b6db5ab41b4c99df89be0d1c4
Alex Liu <LIUAL14@OOCL.COM>
2020-11-16 17:50:24 +0800

    answers

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 // Complete the following procedure, the 'name' variable in all letters to uppercase, output: ' 'HELLO' '.
 var name = 'hello';
 // TODO write your code here...
-
+name = name.toUpperCase();
 
 // Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
 var sentence = 'good afternoon, mr mike.';
 //TODO write your code here...
-
+sentence = sentence.split(" ").map(x => x.substring(0,1).toUpperCase() + x.substring(1)).join(" ");
 
 // Complete the following procedure to extract the amount of money variable, output: 20.
 var money = '￥20';
 //TODO write your code here...
+money = parseInt(money.substring(1));
